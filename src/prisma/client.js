56086cd7d3480b0fdb31c6995dd3939008af42d9
Dinh Van Pham Viet <vietphamdinhvan@gmail.com>
2026-04-26 00:28:12 +0700

    fix: setup prisma adapter and generate client

--- src/prisma/client.js
+++ src/prisma/client.js
@@ -1,20 +1,24 @@
-import pkg from "@prisma/client";
-
-const { PrismaClient } = pkg;
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis;
+
+const adapter =
+  globalForPrisma.prismaPgAdapter ??
+  new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+  });
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "warn", "error"]
-        : ["error"],
+    adapter,
+    log: process.env.NODE_ENV === "development" ? ["warn", "error"] : ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
+  globalForPrisma.prismaPgAdapter = adapter;
 }
 
 export default prisma;
